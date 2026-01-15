ce3c7791ce7e8fb7649327dcb27da1b89d742a9d
miraclePrime322 <pashakov92@Gmail.com>
2026-01-15 15:44:06 +0700

    Изначальный коммит

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+
+    [SerializeField] private float rotationSpeed = 10f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x,0f ,inputVector.y);
+
+        transform.forward = Vector3.Slerp(transform.forward , moveDir ,rotationSpeed * Time.deltaTime);
+
+        transform.position += moveSpeed * moveDir * Time.deltaTime;
+
+        Debug.Log(inputVector);
+    }
+}
